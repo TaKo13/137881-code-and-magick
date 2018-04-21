e5380e0750de99ef3dbb7f05b5bd8688f7f4e62d
t.kotova <patrikklava@gmail.com>
2018-04-21 15:35:17 +0300

    Добавлена возможность перетаскивания диалога; добавлена возможность перетаскивания предметов

--- js/setup.js
+++ js/setup.js
@@ -87,12 +87,22 @@ createFragment(wizards);
 userDialog.querySelector('.setup-similar').classList.remove('hidden');
 
 var setup = document.querySelector('.setup');
+
+var initialDialogPosition = {
+  top: setup.offsetTop,
+  left: setup.offsetLeft
+};
+
 var openSetup = function() {
   setup.classList.remove('hidden');
   document.addEventListener('keydown', onEscKeydown);
 };
 var closeSetup = function() {
   setup.classList.add('hidden');
+
+  setup.style.top = initialDialogPosition.top + 'px';
+  setup.style.left = initialDialogPosition.left + 'px';
+
   document.removeEventListener('keydown', onEscKeydown);
 };
 
@@ -149,3 +159,46 @@ var onWizardEyesClick = function() {
 
 wizardEyes.addEventListener('click', onWizardEyesClick);
 setupFireballWrap.addEventListener('click', onSetupFireballWrapClick);
+
+// Реализуйте возможность перетаскивания предметов:
+var shopElement = document.querySelector('.setup-artifacts-shop');
+var draggedItem = null;
+var artifactsElement = document.querySelector('.setup-artifacts');
+
+shopElement.addEventListener('dragstart', function(evt) {
+  if (evt.target.tagName.toLowerCase() === 'img') {
+    artifactsElement.style.outline = '2px dashed red';
+    draggedItem = evt.target;
+    evt.dataTransfer.setData('text/plain', evt.target.alt);
+  }
+});
+
+artifactsElement.addEventListener('dragover', function(evt) {
+  evt.preventDefault();
+  return false;
+});
+
+shopElement.addEventListener('dragend', function(evt) {
+  evt.preventDefault();
+  artifactsElement.style.outline = '';
+});
+
+artifactsElement.addEventListener('drop', function(evt) {
+  evt.target.style.backgroundColor = '';
+
+  if (evt.target.tagName.toLowerCase() !== 'img') {
+    evt.target.appendChild(draggedItem.cloneNode());
+  }
+
+  evt.preventDefault();
+});
+
+artifactsElement.addEventListener('dragenter', function(evt) {
+  evt.target.style.backgroundColor = 'yellow';
+  evt.preventDefault();
+});
+
+artifactsElement.addEventListener('dragleave', function(evt) {
+  evt.target.style.backgroundColor = '';
+  evt.preventDefault();
+});
